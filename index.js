6dcf96c58b6a55eb5bbaa45afae5075d05a9dc66
Jeevamk <138573828+Jeevamk@users.noreply.github.com>
2024-07-19 18:01:23 +0530

    api connected

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-const express = require ('express')
-const app  = express();
-const mongoose = require ('./server/database/connection')
+const express = require('express')
+const app = express();
 const port = 5001;
 
 
+app.get('/', async (req, res) => {
+    try {
+        const response = await axios.get("https://api.openweathermap.org/data/3.0/onecall", {
+            params: {
+                lat: 11.0645301,
+                lon: 76.0240333,
+                appid: 'ae813b7e4adcfad9ea2a1015f4d49ea1'
+            }
+        });
+        console.log(response.data);
+        res.status(200).json(response.data);
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Unable to fetch the data" });
+    }
+})
 
 
 
 
-
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`running on port ${port}`);
 })
--- server/database/connection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://jeevamk100:jeeva8839@cluster0.pbixvru.mongodb.net/weatherwatch?retryWrites=true&w=majority')
-.then(() =>{
-    console.log(`connect`);
-})
-.catch((err) => {
-    console.log(err);
-})
